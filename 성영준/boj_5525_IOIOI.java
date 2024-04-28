729879bf8b738268f063e71272e1f16b732e44d3
tddudwns1 <tjddudwns1@naver.com>
2024-04-28 12:27:01 +0900

    [BOJ] 5525. IOIOI 1️⃣0️⃣1️⃣0️⃣1️⃣

---
+++ 성영준/boj_5525_IOIOI.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_5525_IOIOI {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int s = Integer.parseInt(br.readLine());
+        String str = br.readLine() + " ";
+
+        int index = 0;
+        int count = 0;
+        portal:
+        while (index < s) {
+            if (str.charAt(index++) != 'I')
+                continue;
+
+            for (int j = 0; j < n - 1; j++) {
+                if (str.charAt(index) != 'O')
+                    continue portal;
+
+                index++;
+
+                if (str.charAt(index++) != 'I')
+                    continue portal;
+            }
+
+            while (true) {
+                if (str.charAt(index) != 'O')
+                    continue portal;
+
+                index++;
+
+                if (str.charAt(index++) != 'I')
+                    continue portal;
+
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
