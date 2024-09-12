809e59295c0905d8b77a7110c37275911902d52a
tddudwns1 <tjddudwns1@naver.com>
2024-09-12 23:22:43 +0900

    [BOJ] 17609. 회문

---
+++ 성영준/boj_17609_회문.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_17609_회문 {
+    static String str;
+    static int left, right;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            str = br.readLine();
+
+            left = 0;
+            right = str.length() - 1;
+
+            sb.append(isPalindrome());
+        }
+
+        System.out.print(sb);
+    }
+
+    private static String isPalindrome() {
+        if (checkCharacter())
+            return "0\n";
+
+        int tempLeft = left++;
+        int tempRight = right;
+        if (str.charAt(left) == str.charAt(right))
+            if (checkCharacter())
+                return "1\n";
+
+        left = tempLeft;
+        right = --tempRight;
+        if (str.charAt(left) == str.charAt(right))
+            if (checkCharacter())
+                return "1\n";
+
+        return "2\n";
+    }
+
+    private static boolean checkCharacter() {
+        while (str.charAt(left) == str.charAt(right))
+            if (++left >= --right)
+                return true;
+
+        return false;
+    }
+}
