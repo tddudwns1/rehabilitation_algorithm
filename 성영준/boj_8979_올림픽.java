064de571de8179d54f72c5258c0f7311c1ede58c
tddudwns1 <tjddudwns1@naver.com>
2024-10-14 22:52:19 +0900

    [BOJ] 8979. 올림픽

---
+++ 성영준/boj_8979_올림픽.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class boj_8979_올림픽 {
+    static class Country implements Comparable<Country>{
+        int num;
+        int gold;
+        int silver;
+        int bronze;
+
+        public Country(int num, int gold, int silver, int bronze) {
+            this.num = num;
+            this.gold = gold;
+            this.silver = silver;
+            this.bronze = bronze;
+        }
+
+        @Override
+        public int compareTo(Country o) {
+            if (gold != o.gold)
+                return Integer.compare(o.gold, gold);
+            if (silver != o.silver)
+                return Integer.compare(o.silver, silver);
+            return Integer.compare(o.bronze, bronze);
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Country[] countries = new Country[n];
+        Country target = null;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int num = Integer.parseInt(st.nextToken());
+            int gold = Integer.parseInt(st.nextToken());
+            int silver = Integer.parseInt(st.nextToken());
+            int bronze = Integer.parseInt(st.nextToken());
+
+            Country country = new Country(num, gold, silver, bronze);
+            countries[i] = country;
+
+            if (num == k)
+                target = country;
+        }
+
+        Arrays.sort(countries);
+
+        System.out.println(binarySearch(countries, target, n));
+    }
+
+    private static int binarySearch(Country[] countries, Country target, int n) {
+        int left = 0;
+        int right = n - 1;
+
+        while(left < right) {
+            int mid = (left + right) >> 1;
+
+            if (countries[mid].compareTo(target) < 0) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left + 1;
+    }
+}
