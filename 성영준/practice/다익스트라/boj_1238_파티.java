88e6feb09d76dd88ca4374512dcb2365f388e166
tddudwns1 <tjddudwns1@naver.com>
2024-10-15 21:33:19 +0900

    dijkstra 풀어보았습니다

---
+++ 성영준/practice/다익스트라/boj_1238_파티.java
@@ -0,0 +1,98 @@
+package practice.다익스트라;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class boj_1238_파티 {
+    static class Node implements Comparable<Node> {
+        int destination;
+        int weight;
+
+        public Node(int destination, int weight) {
+            this.destination = destination;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(weight, o.weight);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+
+        Queue<Node>[][] info = new ArrayDeque[2][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < 2; j++) {
+                info[j][i] = new ArrayDeque<>();
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            info[0][a].add(new Node(b, weight));
+            info[1][b].add(new Node(a, weight));
+        }
+
+        System.out.println(process(n, x, info));
+    }
+
+    private static int process(int n, int x, Queue<Node>[][] info) {
+        int[][] weights = new int[2][n + 1];
+
+        for (int i = 0; i < 2; i++) {
+            dijkstra(n, x, weights[i], info[i]);
+        }
+
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = 0; j < 2; j++) {
+                sum += weights[j][i];
+            }
+            max = Math.max(max, sum);
+        }
+
+        return max;
+    }
+
+    private static void dijkstra(int n, int x, int[] weights, Queue<Node>[] queues) {
+        Queue<Node> pq = new PriorityQueue<>();
+        boolean[] visited = new boolean[n + 1];
+
+        pq.add(new Node(x, 0));
+
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+
+            if (visited[now.destination])
+                continue;
+            visited[now.destination] = true;
+            weights[now.destination] = now.weight;
+
+            while (!queues[now.destination].isEmpty()) {
+                Node next = queues[now.destination].poll();
+
+                if (visited[next.destination])
+                    continue;
+                next.weight += now.weight;
+                pq.add(next);
+            }
+        }
+    }
+}
---
+++ 성영준/practice/다익스트라/boj_1753_최단경로.java
@@ -0,0 +1,86 @@
+package practice.다익스트라;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class boj_1753_최단경로 {
+    static class Edge implements Comparable<Edge> {
+        int destination;
+        int weight;
+
+        public Edge(int destination, int weight) {
+            this.destination = destination;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(weight, o.weight);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+
+        int start = Integer.parseInt(br.readLine());
+
+        Queue<Edge>[] ways = new Queue[v + 1];
+        for (int i = 1; i <= v; i++) {
+            ways[i] = new ArrayDeque<>();
+        }
+
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            ways[from].add(new Edge(to, weight));
+        }
+
+        System.out.println(dijkstra(v, start, ways));
+    }
+
+    private static String dijkstra(int v, int start, Queue<Edge>[] ways) {
+        Queue<Edge> pq = new PriorityQueue<>();
+
+        pq.add(new Edge(start, 0));
+
+        int[] weights = new int[v + 1];
+        boolean[] visited = new boolean[v + 1];
+
+        while (!pq.isEmpty()) {
+            Edge now = pq.poll();
+
+            if (visited[now.destination])
+                continue;
+            visited[now.destination] = true;
+            weights[now.destination] = now.weight;
+
+            while(!ways[now.destination].isEmpty()) {
+                Edge poll = ways[now.destination].poll();
+
+                if (visited[poll.destination])
+                    continue;
+                poll.weight += now.weight;
+                pq.add(poll);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= v; i++) {
+            if (visited[i])
+                sb.append(weights[i]).append("\n");
+            else
+                sb.append("INF\n");
+        }
+        return sb.toString();
+    }
+}
