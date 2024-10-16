5395a27bf48f7763842a97f7e3e3a72edade3acf
tddudwns1 <tjddudwns1@naver.com>
2024-10-16 21:20:06 +0900

    Topological Sorting 풀어보았습니다

---
+++ 성영준/practice/위상정렬/boj_2252_줄세우기.java
@@ -0,0 +1,49 @@
+package practice.위상정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class boj_2252_줄세우기 {
+    static StringBuilder sb = new StringBuilder();
+    static Deque<Integer>[] tallerList;
+    static boolean[] checked;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        tallerList = new ArrayDeque[n + 1];
+        for (int i = 1; i <= n; i++)
+            tallerList[i] = new ArrayDeque<>();
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int taller = Integer.parseInt(st.nextToken());
+            int shorter = Integer.parseInt(st.nextToken());
+
+            tallerList[shorter].add(taller);
+        }
+
+        checked = new boolean[n + 1];
+
+        for (int i = 1; i <= n; i++)
+            lineUp(i);
+
+        System.out.println(sb);
+    }
+
+    private static void lineUp(int now) {
+        if (checked[now]) return;
+
+        while (!tallerList[now].isEmpty())
+            lineUp(tallerList[now].remove());
+
+        checked[now] = true;
+        sb.append(now).append(" ");
+    }
+}
