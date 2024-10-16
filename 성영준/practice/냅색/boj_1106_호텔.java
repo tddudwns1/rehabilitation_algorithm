53480e49d555f80d39d16abdcb05fbf12d4936cb
tddudwns1 <tjddudwns1@naver.com>
2024-10-16 23:32:18 +0900

    Knapsack 풀어보았습니다

---
+++ 성영준/practice/냅색/boj_1106_호텔.java
@@ -0,0 +1,42 @@
+package practice.냅색;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_1106_호텔 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int c = Integer.parseInt(st.nextToken()) + 100;
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] costs = new int[c];
+        Arrays.fill(costs, Integer.MAX_VALUE);
+        costs[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < c - w; j++) {
+                if (costs[j] != Integer.MAX_VALUE) {
+                    int newCost = j + w;
+                    costs[newCost] = Math.min(costs[newCost], costs[j] + v);
+                }
+            }
+        }
+
+        int answer = Integer.MAX_VALUE;
+        for (int j = c - 100; j < c; j++) {
+            answer = Math.min(answer, costs[j]);
+        }
+
+        System.out.println(answer);
+    }
+}
---
+++ 성영준/practice/냅색/boj_12865_평범한배낭.java
@@ -0,0 +1,49 @@
+package practice.냅색;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class boj_12865_평범한배낭 {
+    static class Product {
+        int w;
+        int v;
+
+        public Product(int w, int v) {
+            this.w = w;
+            this.v = v;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Queue<Product> pq = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            pq.add(new Product(w, v));
+        }
+
+        int[] cost = new int[k + 1];
+        while (!pq.isEmpty()) {
+            Product now = pq.poll();
+
+            for (int i = k; i >= now.w; i--) {
+                cost[i] = Math.max(cost[i], cost[i - now.w] + now.v);
+            }
+        }
+
+        System.out.println(cost[k]);
+    }
+}
