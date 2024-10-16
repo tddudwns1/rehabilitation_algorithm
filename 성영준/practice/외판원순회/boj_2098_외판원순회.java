48fdd5af0a6208ab6d5b5f42939c52b5a651cabc
tddudwns1 <tjddudwns1@naver.com>
2024-10-16 16:46:26 +0900

    Travelling Salesman Problem 풀어보았습니다

---
+++ 성영준/practice/외판원순회/boj_2098_외판원순회.java
@@ -0,0 +1,57 @@
+package practice.외판원순회;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_2098_외판원순회 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] costs = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                costs[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        System.out.println(process(n, costs));
+    }
+
+    private static int process(int n, int[][] costs) {
+        int allVisited = (1 << n) - 1;
+        int lastCountryNumber = n - 1;
+        int maxCost = 16 * 1_000_000 + 1;
+
+        int[][] visited = new int[n][allVisited];
+
+        return dfs(0, 1, visited, costs, allVisited, lastCountryNumber, maxCost);
+    }
+
+    private static int dfs(int now, int nowVisited, int[][] visited, int[][] costs, int allVisited, int lastCountryNumber, int maxCost) {
+        if (nowVisited == allVisited) {
+            if (costs[now][0] == 0)
+                return maxCost;
+            return costs[now][0];
+        }
+
+        if (visited[now][nowVisited] != 0)
+            return visited[now][nowVisited];
+
+        int minCost = maxCost;
+
+        for (int i = lastCountryNumber; i >= 0; i--) {
+            int nowCountry = 1 << i;
+            if ((nowVisited & nowCountry) != 0)
+                continue;
+            if (costs[now][i] == 0)
+                continue;
+
+            minCost = Math.min(minCost, costs[now][i] + dfs(i, nowVisited | nowCountry, visited, costs, allVisited, lastCountryNumber, maxCost));
+        }
+        return visited[now][nowVisited] = minCost;
+    }
+}
